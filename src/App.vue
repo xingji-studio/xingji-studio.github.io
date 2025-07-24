91757149d56b4e2e3e8cd6011d3cab5fe81a4e13
W9pi3cZ1 <ghg13368@163.com>
2025-07-24 17:26:37 +0800

    Added dynamic NavName supports

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,23 @@
 <script setup lang="ts">
+import { ref, watch } from 'vue';
 import Navigation from './components/Navigation.vue'
+import { useRoute } from 'vue-router';
+let navName = ref('星际工作室');
+const route = useRoute();
+
+watch(
+  () => route.meta.navName,
+  (newName) => {
+    if (newName) navName.value = newName as string;
+    else navName.value = '星际工作室';
+  },
+  { immediate: true }
+);
 </script>
 
 <template>
   <div class="min-h-screen bg-gray-900">
-    <Navigation />
+    <Navigation :nav-name=navName />
     <router-view></router-view>
   </div>
 </template>
